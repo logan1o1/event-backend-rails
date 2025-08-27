6b87a0f3fbe006f8e23dcd2cc06ff087222239c9
logan1o1 <saswatm2706@gmail.com>
2025-08-27 08:54:12 -0700

    cors fixed

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,10 +1,10 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:5173'
+    origins "http://localhost:5173", "https://event-hub-yg55.onrender.com"
 
-    resource '*',
+    resource "*",
       headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      expose: ['Authorization'] 
+      methods: [ :get, :post, :put, :patch, :delete, :options, :head ],
+      expose: [ "Authorization" ]
   end
 end
